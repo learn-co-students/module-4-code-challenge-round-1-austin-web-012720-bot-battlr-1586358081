7c0739591113926c3776e3c6843921c39b5cfcfc
qudroturkey <reesemb1@gmail.com>
2020-04-08 11:55:12 -0500

    mod 4 code challenge

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends Component {
-  //start here with your code for step one
+  constructor() {
+    super()
+    this.state = {
+      bots: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:6001/bots')
+      .then(resp => resp.json())
+      .then(bots => this.setState({ bots }))
+  }
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    return <div>
+    <YourBotArmy/>
+    <BotCollection bots={this.state.bots}/>
+    </div>;
   }
 }
 
