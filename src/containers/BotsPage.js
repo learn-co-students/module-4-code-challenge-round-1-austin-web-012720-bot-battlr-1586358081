4987a55f543e740db4ffec254ca7fd3432db0722
Dane-Dawson <Dane.P.Dawson@gmail.com>
2020-04-08 11:21:26 -0500

    it's working at least

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,59 @@
 import React, { Component } from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
+const botsAPI = `http://localhost:6001/bots`;
 class BotsPage extends Component {
-  //start here with your code for step one
+  state = {
+    bots: [],
+    armyBots: [],
+  };
+
+  toggleBotToArmy = (bot) => {
+    let index = this.state.armyBots.indexOf(bot);
+    if (index < 0) {
+      this.setState({ armyBots: [...this.state.armyBots, bot] });
+    } 
+  };
+
+  removeBotFromArmy = (bot) => {
+    let index = this.state.armyBots.indexOf(bot);
+    this.setState(this.state.armyBots.splice(index, 1));
+  }
+
+  deleteBot = (bot) => {
+    fetch(botsAPI + "/" + bot.id, {
+      method: "DELETE",
+    });
+  };
+
+  removeBot = (bot) => {
+    // let confirmation = confirm("Are you sure?");
+    // alert (confirmation)
+    let index = this.state.armyBots.indexOf(bot);
+    if (index > -1) {
+      this.setState(this.state.armyBots.splice(index, 1));
+    }
+    this.deleteBot(bot)
+    this.fetchBots()
+  }
+  fetchBots = () => {
+    fetch(botsAPI)
+      .then((res) => res.json())
+      .then((bots) => this.setState({ bots }));
+  };
+
+  componentDidMount() {
+    this.fetchBots();
+  }
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    return (
+      <div>
+        <YourBotArmy bots={this.state.armyBots} addBot={this.removeBotFromArmy} removeBot={this.removeBot}/>
+        <BotCollection bots={this.state.bots} addBot={this.toggleBotToArmy} removeBot={this.removeBot} />
+      </div>
+    );
   }
 }
 
