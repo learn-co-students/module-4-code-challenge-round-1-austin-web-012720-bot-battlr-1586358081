768aaed8a60b891b90b739121c755b95b1a62105
github username <github email address>
2020-04-08 11:55:50 -0500

    maylene

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends Component {
-  //start here with your code for step one
+  constructor(){
+    super()
+    this.state = {
+      allBots: [],
+      botArmy: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/bots')
+    .then(response => response.json())
+    .then(results => this.setState({ allBots: results}))  
+  }
+
+  addToBotArmy = (bot) => {
+    if(!this.state.botArmy.includes(bot)){
+      this.setState({ botArmy: [...this.state.botArmy, bot] })
+    }
+  }
+
+  releaseBot = (botToDelete) => {
+    const newArmy = this.state.botArmy.filter(bot => bot !== botToDelete)
+    this.setState({botArmy: newArmy})
+  }
+
+  deleteBot = (bot) => {
+    const botId = bot.id
+    fetch(`http://localhost:6001/bots/${botId}`, {
+      method: 'DELETE',
+
+    })
+  }
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    return (
+      <div>
+        <YourBotArmy botArmy={this.state.botArmy}  />
+        <BotCollection bots={this.state.allBots} addToBotArmy={this.addToBotArmy} deleteBot={this.deleteBot}/>
+      </div>
+    );
   }
 }
 
